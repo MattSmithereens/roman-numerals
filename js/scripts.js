d663c1080c890a83be9342ea46a918c458bea608
Eric Shull and Matt Smith <eric@>
2018-06-07 10:06:39 -0700

    establish js pseudocode employing propsed numeric rendering logic for nums 1-9

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+var functOnes = function() {
+  if (onesInput === 1) {
+    return = ("I")
+  };
+  else if (onesInput === 2) {
+    return = ("II")
+  };
+  else if (onesInput === 3) {
+    return = ("III")
+  };
+  else if(onesInput === 4) {
+    return = ("IV")
+  };
+  else if(onesInput === 5) {
+    return = ("V")
+  };
+  else if(onesInput === 6) {
+    return = ("VI")
+  };
+  else if(onesInput === 7) {
+    return = ("VII")
+  };
+  else if(onesInput === 8) {
+    return = ("VIII")
+  };
+  else if(onesInput === 9) {
+    return = ("IX")
+  };
+  console.log(onesInput);
+};
+// var functFour
+// var functFivetoEight
+// var functNine
